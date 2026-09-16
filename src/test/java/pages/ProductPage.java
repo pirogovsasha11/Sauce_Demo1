ff463f544be2cf257f0ae7651d8b3ecdeb6a049b
Pirogov Alexander <pirogovsasha11@gmail.com>
2026-09-16 23:32:58 +0300

    Added to this branch: - Allure Report

--- src/test/java/pages/ProductPage.java
+++ src/test/java/pages/ProductPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -15,37 +16,45 @@ public class ProductPage extends BasePage {
         super(driver);
     }
 
+    @Step("Отображение страницы")
     public boolean isPageTitleVisible() {
         return driver.findElement(pageTitle).isDisplayed();
     }
 
+    @Step("Отображение заголовка страницы")
     public String getPageTitle() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(pageTitle));
         return driver.findElement(pageTitle).getText();
     }
 
+    @Step("Добавление товара в корзину (String)")
     public void addGoodsToCart(String goodsName) {
         By addToCartButton = By.xpath(ADD_CART_PATTERN.formatted(goodsName));
         driver.findElement(addToCartButton).click();
     }
 
+    @Step("Добавление товара в корзину (Int)")
     public void addGoodsToCart(int goodsIndex) {
         By addToCartButton = By.xpath("//button[text()='Add to cart']");
         driver.findElements(addToCartButton).get(goodsIndex).click();
     }
 
+    @Step("Отображение добавленного товара")
     public boolean isCartBadgeVisible() {
         return driver.findElement(cartBadge).isDisplayed();
     }
 
+    @Step("Отображение количества добавленных товаров")
     public String checkCountersValue() {
         return driver.findElement(cartBadge).getText();
     }
 
+    @Step("Сравнение добавленных товаров по цветовой идикации")
     public String checkCountersColor() {
         return driver.findElement(cartBadge).getCssValue("background-color");
     }
 
+    @Step("Переход в корзину")
     public void switchToCart() {
         driver.findElement(cartLink).click();
     }
